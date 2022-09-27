0a0c428c3807aee0d1eb7add55ac4050c29f62ee
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-09-27 19:41:53 +0300

    completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, onClickCategory}) {
+  // console.log(categories)
+  const categoryButtons = categories.map((category, index) => <button key={index} value={category} onClick={onClickCategory}>{category}</button>)
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Task() {
+function Task({text, category, onDelete}) {
+
+  function handleDeleteChange() {
+    onDelete(text);
+    // console.log(text)
+  }
+ 
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDeleteChange}>X</button>
     </div>
   );
 }
